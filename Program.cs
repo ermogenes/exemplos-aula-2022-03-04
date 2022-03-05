5f4c59ddd3d30e621b2e717705005b359eadcc39
Ermogenes Palacio <ermogenes@gmail.com>
2022-03-04 21:40:07 -0300

    Finaliza o exemplo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Limpa a tela
+Console.Clear();
+
+// Exibe Ermogenes na cor certa
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Ermogenes");
+
+// Espera uma tecla
+Console.ResetColor();
+Console.Write("Pressione uma tecla para continuar...");
+Console.ReadKey();
+Console.WriteLine();
+
+// Exibe Neri
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Neri");
+
+// Volta a cor padrão
+Console.ResetColor();
